f5b3434118740bfd9a85288cf92a5ec1e9dd2559
Bushra12 <Bushra_Anjum@epam.com>
2017-11-09 15:58:07 +0530

    Correction based on metor comments

--- Task2/wordCount.js
+++ Task2/wordCount.js
@@ -1,9 +1,6 @@
 function countWords(str) {
-    var count = 0;
-    words = str.split(" ");
-    for (i = 0; i < words.length; i++) {
-        if (words[i] != "")
-            count += 1;
-    }
-    return count;
-}
+    return str
+        .split(/\s+/)
+        .filter(function (text) { return text != '' })
+        .length;
+} 
